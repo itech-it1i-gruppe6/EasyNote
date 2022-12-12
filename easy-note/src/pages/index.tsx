9e4ad2c36f9b11e644cfef3011c65acc7469d87c
Konstantinos <kkaryofilis@googlemail.com>
2022-12-12 13:25:41 +0100

    feat: provider & signin

--- easy-note/src/pages/index.tsx
+++ easy-note/src/pages/index.tsx
@@ -69,8 +69,6 @@ const AuthShowcase: React.FC = () => {
     { enabled: sessionData?.user !== undefined },
   );
 
-  //sessionData ? () => signOut() : () => signIn()
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
@@ -79,7 +77,7 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={()=> some.push("/login")}
+        onClick={ sessionData ? () => signOut() : () => signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
