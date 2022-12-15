891b0f48fa92181c32f0a586c1f111048e86be33
Konstantinos <kkaryofilis@googlemail.com>
2022-12-15 10:43:24 +0100

    feat:added cypress

---
+++ easy-note/cypress/e2e/tests/example.test.ts
@@ -0,0 +1,9 @@
+import { Cipher } from "crypto"
+
+describe('example test', () => {
+    beforeEach(() => {
+            cy.visit("http://localhost:3000/")
+
+    })
+    it("some", () => {cy.wait(10)})
+})
